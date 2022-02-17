5b3fc81171894a2e8706310dbbe03f2e207e13c4
Neka <nekoanchor@yahoo.fr>
2022-02-17 11:18:35 +0100

    feat: add login route

--- routes/users.js
+++ routes/users.js
@@ -30,6 +30,37 @@ app.post("/user/register", (req, res) => {
   })
 })
 
+app.post("/login", (req, res) => {
+
+  User.findOne({ where: { username: req.body.username } }).then(user => {
+
+    if(!user){
+      const message = "This user does not exist.";
+      return res.status(404).json({ message })
+    }
+
+    bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+      if(!isPasswordValid){
+        const message = "Wrong password.";
+        return res.status(401).json({ message })
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        privateKey,
+        { expiresIn: "24h" }
+      )
+
+      const message = "User has been successfully logged in";
+      return res.json({ message, data: user, token })
+    })
+  })
+  .catch(error => {
+    const message = "User has NOT been logged in. Please try again.";
+    return res.json({ message, data: error })
+  })
+})
+
   //INDEX (Display All)
   app.get("/users", (req, res) => {
     //Check if Admin

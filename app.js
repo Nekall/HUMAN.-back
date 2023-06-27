4240030d950c85dc4e21a5ca06e9b90165d530e7
Neka <nekoanchor@yahoo.fr>
2023-06-27 10:43:29 +0200

    setup: chnate firebase to mongoose for mongodb

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.info("Hello World!");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is ready on port: http://localhost:${PORT}`);
+});

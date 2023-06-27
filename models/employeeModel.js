9d0f1f570d634f1e2faa12dadf4bae0b8b39009e
Neka <nekoanchor@yahoo.fr>
2023-06-27 15:33:06 +0200

    feat: add all models

---
+++ models/employeeModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+});
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+export default Employee;
---
+++ models/ticketModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+  status: {
+    type: String,
+    enum: ["Open", "In Progress", "Closed"],
+    default: "Open",
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employee",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;

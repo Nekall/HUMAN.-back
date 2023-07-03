f6250986de48a11d39a7727397f23ca9b7de170e
Neka <nekoanchor@yahoo.fr>
2023-07-03 10:35:26 +0200

    feat: better return messages product

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,18 +12,26 @@ export const createProduct = async (req, res) => {
     });
 
     const savedProduct = await product.save();
-    res.status(201).json(savedProduct);
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product: savedProduct,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error creating product", error: error.message });
+    res.status(500).json({ success: false, message: "Error creating product", error: error.message });
   }
 };
 
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json(products);
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched all products",
+      products,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error getting products", error: error.message });
+    res.status(500).json({ success: false, message: "Error getting products", error: error.message });
   }
 };
 
@@ -32,11 +40,15 @@ export const getProductById = async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     if (!product) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.status(404).json({ success: false, message: "Product not found" });
     }
-    res.status(200).json(product);
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched product",
+      product,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error getting product", error: error.message });
+    res.status(500).json({ success: false, message: "Error getting product", error: error.message });
   }
 };
 
@@ -52,11 +64,15 @@ export const updateProduct = async (req, res) => {
     );
 
     if (!product) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.status(404).json({ success: false, message: "Product not found" });
     }
-    res.status(200).json(product);
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated product",
+      product,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error updating product", error: error.message });
+    res.status(500).json({ success: false, message: "Error updating product", error: error.message });
   }
 };
 
@@ -65,10 +81,10 @@ export const deleteProduct = async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id);
     if (!product) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.status(404).json({ success: false, message: "Product not found" });
     }
-    res.status(200).json({ message: "Product deleted successfully" });
+    res.status(200).json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Error deleting product", error: error.message });
+    res.status(500).json({ success: false, message: "Error deleting product", error: error.message });
   }
 };

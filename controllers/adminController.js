07753d8504abda312ff895243d4c17feebaf2ad0
Neka <nekoanchor@yahoo.fr>
2023-07-03 10:43:37 +0200

    feat: better return messages admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,18 +11,38 @@ export const createAdmin = async (req, res) => {
     });
 
     const savedAdmin = await admin.save();
-    res.status(201).json(savedAdmin);
+    res.status(201).json({
+      success: true,
+      message: "Successfully created admin",
+      data: savedAdmin,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error creating admin", error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error creating admin",
+        error: error.message,
+      });
   }
 };
 
-export const getAllAdmins = async (req, res) => {
+export const getAllAdmins = async (_, res) => {
   try {
     const admins = await Admin.find();
-    res.status(200).json(admins);
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched all admins",
+      data: admins,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error getting admins", error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error getting admins",
+        error: error.message,
+      });
   }
 };
 
@@ -31,11 +51,23 @@ export const getAdminById = async (req, res) => {
     const { id } = req.params;
     const admin = await Admin.findById(id);
     if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Admin not found" });
     }
-    res.status(200).json(admin);
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched admin",
+      data: admin,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error getting admin", error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error getting admin",
+        error: error.message,
+      });
   }
 };
 
@@ -51,11 +83,23 @@ export const updateAdmin = async (req, res) => {
     );
 
     if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Admin not found" });
     }
-    res.status(200).json(admin);
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated admin",
+      data: admin,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error updating admin", error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error updating admin",
+        error: error.message,
+      });
   }
 };
 
@@ -64,10 +108,20 @@ export const deleteAdmin = async (req, res) => {
     const { id } = req.params;
     const admin = await Admin.findByIdAndDelete(id);
     if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Admin not found" });
     }
-    res.status(200).json({ message: "Admin deleted successfully" });
+    res
+      .status(200)
+      .json({ success: true, message: "Admin deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Error deleting admin", error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error deleting admin",
+        error: error.message,
+      });
   }
 };
